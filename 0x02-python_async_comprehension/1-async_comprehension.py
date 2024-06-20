2791434d4431b8d5d6649feeab4d001f3adf468b
RangiraDave <rangiradev666@gmail.com>
2024-06-20 19:50:45 +0200

    Asynchronous operations are awsome.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,8 +8,12 @@ import random
 from typing import List
 
 
-async def async_generator() -> List[float]:
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
     """
-    Coroutine that will yield a random number between 0 and 10
+    Coroutine that will collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
     """
-    return [random.uniform(0, 10) for _ in range(10)]
+    return [num async for num in async_generator()]
