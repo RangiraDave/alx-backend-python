d39b597a916c316e80d7b2b6391e15e86a815fcd
RangiraDave <rangiradev666@gmail.com>
2024-06-20 19:55:45 +0200

    Corrected shebung.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,6 +11,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+    Function that measures the runtime
+    """
     start_time = time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time()
